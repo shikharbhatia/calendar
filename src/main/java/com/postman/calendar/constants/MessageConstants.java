f5093cb7299c66668b6c4e69bbf106985c48578c
Shikhar Bhatia <shikharbhatia@Shikhars-MBP.domain.name>
2020-04-05 15:43:39 +0530

    Added Message Constants and Application Configuration

---
+++ src/main/java/com/postman/calendar/constants/MessageConstants.java
@@ -0,0 +1,11 @@
+package com.postman.calendar.constants;
+
+public class MessageConstants {
+    public static final String SUCCESSFUL_REGISTRATION = "Registration is Successful.";
+    public static final String USER_EXISTS = "User Already Exists.";
+    public static final String SLOT_DEFINED_SUCCESS = "Available Slots Successfully Defined.";
+    public static final String SLOT_ALREADY_DEFINED = "Available Slots Already Defined.";
+    public static final String INCORRECT_USER = "Incorrect User Requested.";
+    public static final String SLOT_ALREADY_BOOKED = "Slot Already Booked or Incorrect Slot ID Requested.";
+    public static final String SLOT_BOOK_SUCCESS = "Slot Booked Successfully.";
+}
